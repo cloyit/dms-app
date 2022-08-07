6652329428ecb64073a4f72a04b627bd57da0f0a
SODA <534490138@qq.com>
2022-08-07 22:19:34 +0800

    employee方法创建

---
+++ src/main/java/com/example/drive/entity/Employee.java
@@ -0,0 +1,40 @@
+package com.example.drive.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+public class Employee implements Serializable {
+    private static final long serialVersionUID=1L;
+    @ApiModelProperty("员工的ID")
+    private Long id;
+    @ApiModelProperty("用户名")
+    private String username;
+    @ApiModelProperty("姓名")
+    private String name;
+    @ApiModelProperty("密码")
+    private String password;
+    @ApiModelProperty("手机号")
+    private String phone;
+    @ApiModelProperty("性别")
+    private String sex;
+    @ApiModelProperty("身份证号码")
+    private String idNumber;
+    @ApiModelProperty("状态")
+    private Integer status;
+
+    //公共字段，所有表基本都有
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
+}
---
+++ src/main/java/com/example/drive/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.example.drive.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.drive.entity.Employee;
+
+/**
+ * 员工功能服务接口
+ */
+public interface EmployeeService extends IService<Employee> {
+}
