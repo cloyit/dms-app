3ea974c19fce20317334dddecc404b19d7f0fa99
bowlmeat <63241002+bowlmeat@users.noreply.github.com>
2022-08-08 09:06:34 +0800

    实体类上传，包括分组类与帖子类

--- src/main/java/com/example/drive/entity/Category.java
+++ src/main/java/com/example/drive/entity/Category.java
@@ -1,6 +1,5 @@
 package com.example.dmsapp.entity;
 
-
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
@@ -9,41 +8,44 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 
 /**
- * 帖子实体类
+ * 分类实体类
  */
 @Data
-public class Post implements Serializable {
+public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
     /**
-     * 帖子id
+     * 类别id
      */
     private Integer id;
 
     /**
-     * 类别id
+     * 分类名称
      */
-    private Integer category;
+    private String name;
 
     /**
-     * 审核状态
+     * 顺序
      */
-    private Integer status;
-
-    /**
-     * 帖子内容
-     */
-    private String content;
+    private Integer sort;
 
     /**
      * 创建时间
      */
     @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
-
+    /**
+     * 更新时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
     /**
      * 创建人
      */
     @TableField(fill = FieldFill.INSERT)
     private Integer createUser;
-
+    /**
+     * 修改人
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Integer updateUser;
 }
