dd1312c377686494ccc1812c56602b7cabed0a5a
bowlmeat <63241002+bowlmeat@users.noreply.github.com>
2022-08-08 09:15:01 +0800

    帖子与分组的service接口

---
+++ src/main/java/com/example/drive/service/CategoryService.java
@@ -0,0 +1,8 @@
+package com.example.dmsapp.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.dmsapp.entity.Category;
+
+public interface CategoryService extends IService<Category> {
+    // void remove(Long id);
+}
