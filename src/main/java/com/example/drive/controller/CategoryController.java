d183c374db8eb1bb679b5f37fc06fa4cadbf715d
bowlmeat <63241002+bowlmeat@users.noreply.github.com>
2022-08-08 11:14:55 +0800

    Update CategoryController.java

--- src/main/java/com/example/drive/controller/CategoryController.java
+++ src/main/java/com/example/drive/controller/CategoryController.java
@@ -1,4 +1,4 @@
-package com.example.dmsapp.controller;
+package com.example.drive.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
